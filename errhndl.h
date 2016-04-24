32c89eb393e678397d56513f61114b835f1fa93c
Aaron McSwain <myutube32@gmail.com>
2016-04-24 15:51:23 -0400

    Added files via upload

---
+++ errhndl.h
@@ -0,0 +1,19 @@
+/* This file has no copyright assigned and is placed in the Public Domain. */
+#ifndef ERRHNDL_H
+#define ERRHNDL_H
+
+#include <setjmp.h>
+
+/* "Internal" structure, only access through provided macros */
+typedef struct {
+	jmp_buf buffer;
+	int type, in_try_block;
+} errinfo;
+
+#define GETERR(err) ((err).type)
+
+#define ERRTHROW(err, t) do{ if((err).in_try_block){ longjmp((err).buffer, (err).type = (t)); } }while(0)
+#define ERRTRY(err, body) if((((err).in_try_block = 1) == 2) || (!setjmp((err).buffer))){ body (err).in_try_block = 0; }
+#define ERRCATCH(err, body) else{ (err).in_try_block = 0; body }
+
+#endif
