baf2358f650147135bfa3b8fce5a83bad9254130
Aaron McSwain <myutube32@gmail.com>
2016-04-26 12:57:56 -0400

    Update errhndl.h

--- errhndl.h
+++ errhndl.h
@@ -13,7 +13,7 @@ typedef struct {
 #define ERRGET(err) ((err).type)
 
 #define ERRTHROW(err, t) do{ if((err).in_try_block){ longjmp((err).buffer, (err).type = (t)); } }while(0)
-#define ERRTRY(err, body) if((((err).in_try_block = 1) == 2) || (!setjmp((err).buffer))){ body (err).in_try_block = 0; }
+#define ERRTRY(err, body) do{ (err).in_try_block = 1; }while(0); if((!setjmp((err).buffer))){ body (err).in_try_block = 0; }
 #define ERRCATCH(err, body) else{ (err).in_try_block = 0; body }
 
 #endif
